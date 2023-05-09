7053a2f5c00f77743cf0629bc28b59fbf070e47f
Qboy <Qboy@qq.com>
2023-05-09 21:43:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @author Qxj
+ * @version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
